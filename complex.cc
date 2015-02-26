fcb259f26c5754790ce24a6f8d8a26a08eda3f97
Keith Lantz <klantz81@gmail.com>
2015-02-26 16:15:13 -0500

    initial commit

---
+++ complex.cc
@@ -0,0 +1,30 @@
+#include "complex.h"
+
+complex::complex() : a(0.0), b(0.0) { }
+complex::complex(float a, float b) : a(a), b(b) { }
+complex complex::conj() { return complex(this->a, -this->b); }
+
+complex complex::operator*(const complex& c) const {
+	return complex(this->a*c.a - this->b*c.b, this->a*c.b + this->b*c.a);
+}
+
+complex complex::operator+(const complex& c) const {
+	return complex(this->a + c.a, this->b + c.b);
+}
+
+complex complex::operator-(const complex& c) const {
+	return complex(this->a - c.a, this->b - c.b);
+}
+
+complex complex::operator-() const {
+	return complex(-this->a, -this->b);
+}
+
+complex complex::operator*(const float c) const {
+	return complex(this->a*c, this->b*c);
+}
+
+complex& complex::operator=(const complex& c) {
+	this->a = c.a; this->b = c.b;
+	return *this;
+}
---
+++ complex.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class complex {
+  private:
+  protected:
+  public:
+	float a, b;
+	complex();
+	complex(float a, float b);
+	complex conj();
+	complex operator*(const complex& c) const;
+	complex operator+(const complex& c) const;
+	complex operator-(const complex& c) const;
+	complex operator-() const;
+	complex operator*(const float c) const;
+	complex& operator=(const complex& c);
+};
+
+#endif
